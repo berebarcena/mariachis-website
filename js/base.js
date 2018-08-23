161d1efcf3401744ab62fd9e80cd55d8a82d7774
Berenice <berebarcenarm@gmail.com>
2018-08-23 16:07:35 +0200

    added animation to scroll cta on home

--- js/base.js
+++ js/base.js
@@ -1,18 +1,17 @@
 $(document).ready(() => {
+  //scroll sideways
   $("body")
     .mousewheel(function (evt, delta) {
       evt.preventDefault();
       window.scrollBy(delta * -20, 0);
     });
 
+  //open/close navigation
   var isNavAnimating = false;
-
-  //open/close lateral navigation
   $(".nav_trigger").on("click", function (event) {
     event.preventDefault();
     //stop if nav animation is running
     if (!isNavAnimating) {
-
       $("body").toggleClass("navigation-is-open");
       $(".navigation-wrapper").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function () {
         //animation is over
